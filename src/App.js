7ff5046da3c723ebdd0f797649a893e9cf6794c0
Håvard Anda Estensen <haavard.ae@gmail.com>
2017-09-24 17:29:55 -0700

    Add material-ui and make choices buttons

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Button from 'material-ui/Button';
 import './App.css';
 
 class App extends Component {
@@ -7,11 +8,11 @@ class App extends Component {
       <div className="App">
         <h1>What do you want to make tonight?</h1>
         <p className="Choices">
-          Asian
-          Fish
-          Italian
-          French
-          Vegan
+          <Button>Asian</Button>
+          <Button>Fish</Button>
+          <Button>Italian</Button>
+          <Button>French</Button>
+          <Button>Vegan</Button>
         </p>
         <p className="Meals">
           Meal #1
