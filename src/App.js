1d699b18c05858ae125cf24b2ab3e29288c3a88f
Håvard Anda Estensen <haavard.ae@gmail.com>
2017-09-26 15:32:51 -0700

    Add choices to state and add isSelected

--- src/App.js
+++ src/App.js
@@ -3,28 +3,29 @@ import Button from 'material-ui/Button';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    // this.onClick.bind(this);
+    this.state = {
+      choices: [
+        { name: "Asian", isSelected: false },
+        { name: "Fish", isSelected: false },
+        { name: "Italian", isSelected: false },
+        { name: "French", isSelected: false },
+        { name: "Vegan", isSelected: false },
+      ]
+    };
+  }
 
   selectButton = () =>
     console.log("Button selected!");
-
-  choices = [
-    "Asian",
-    "Fish",
-    "Italian",
-    "French",
-    "Vegan"
-  ];
-
-  listChoices = this.choices.map((choice) =>
-    <Button onClick={this.selectButton}>{choice}</Button>
-  );
 
   render() {
     return (
       <div className="App">
         <h1>What do you want to make tonight?</h1>
         <p className="Choices">
-          {this.listChoices}
+          {this.state.choices.map(( choice, index ) => <Button key={index} onClick={this.selectButton}>{choice.name}</Button>)}
         </p>
         <p className="Meals">
           Meal #1
